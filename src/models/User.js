0a6217bd41f053b3038148a12e78f9e1046134d5
George Apostolov <george@livedemo.ai>
2026-03-23 18:31:21 -0400

    Update User model to use bcryptjs instead of bcrypt and add GitHub actions config

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const options = {
   discriminatorKey: 'userType',
   strict: true,
@@ -58,39 +58,38 @@ const UserSchema = new mongoose.Schema({
  * Compare the passed password with the value in the database. A model method.
  *
  * @param {string} password
- * @returns {object} callback
+ * @param {function} callback
  */
 UserSchema.methods.comparePassword = function comparePassword(password, callback) {
-  bcrypt.compare(password, this.password, callback)
+  bcrypt.compare(password, this.password)
+    .then((isMatch) => {
+      callback(null, isMatch)
+    })
+    .catch((err) => {
+      callback(err, false)
+    })
 }
 
 
 /**
  * The pre-save hook method.
  */
-UserSchema.pre('save', function saveHook(next) {
+UserSchema.pre('save', async function saveHook(next) {
   const user = this
 
   // proceed further only if the password is modified or the user is new
   if (!user.isModified('password')) return next()
 
+  try {
+    const salt = await bcrypt.genSalt()
+    const hash = await bcrypt.hash(user.password, salt)
 
-  return bcrypt.genSalt((saltError, salt) => {
-    if (saltError) {
-      return next(saltError)
-    }
+    user.password = hash
 
-    return bcrypt.hash(user.password, salt, (hashError, hash) => {
-      if (hashError) {
-        return next(hashError)
-      }
-
-      // replace a password string with hash value
-      user.password = hash
-
-      return next()
-    })
-  })
+    return next()
+  } catch (error) {
+    return next(error)
+  }
 })
 
 
